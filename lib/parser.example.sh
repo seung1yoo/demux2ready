4fff243f7de893f44208fa1bf37baa6d76ddc535
Seung-il Yoo <seung1.yoo@gamil.com>
2023-09-27 15:23:02 +0900

    example command lines move to var/command.log

--- lib/parser.example.sh
+++
@@ -1,4 +0,0 @@
-
-
-python /storage2/User/siyoo/module/demux2ready/lib/parser.py fastp --outprefix MultiParser.Fastp.Ready --target-dir readyfastq
-python /storage2/User/siyoo/module/demux2ready/lib/parser.py fastp --outprefix MultiParser.Fastp.Clean --target-dir cleanfastq
